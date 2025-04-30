d4102861c2a1d289c5852301a9d03c39dfbfac30
rrichley <94038906+rrichley@users.noreply.github.com>
2025-04-30 15:28:58 +0100

    Update main.tf

--- infra/main.tf
+++ infra/main.tf
@@ -2,7 +2,20 @@ provider "azurerm" {
   features {}
 }
 
-resource "azurerm_resource_group" "core" {
-  name     = "${var.project_name}-rg"
-  location = var.location
+data "azurerm_storage_account" "quote_sa" {
+  name                = "quoteplatformsa"
+  resource_group_name = "quote-platform-rg"
+}
+
+data "azurerm_storage_queue" "quote_requests" {
+  name                 = "quote-requests"
+  storage_account_name = data.azurerm_storage_account.quote_sa.name
+}
+
+output "storage_account_id" {
+  value = data.azurerm_storage_account.quote_sa.id
+}
+
+output "queue_url" {
+  value = data.azurerm_storage_queue.quote_requests.id
 }
